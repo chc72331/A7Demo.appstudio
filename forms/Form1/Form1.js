96b4bc86b2b8992084f50a9a0b16d4ecbef42db6
chc72331 <chc72331@creighton.edu>
2021-02-23 16:22:15 -0600

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,14 +1,22 @@
 // this is some code to test GHD 
 // and Repository process
 
-/*
+
 let myName = "Cindy"
 console.log(`Hello, my name is ${myName}.`)
 console.log(`Welcome to Monday Demos`)
-*/
 
-/*
+
+
 let dogName= ""
 dogName = prompt("Enter the name of your favorite dog: ")
 console.log(`Welcome ${dogName}, it is good to have you here.`)
-*/
+
+// let's create some problems
+
+let myClass = 'My favorite class is BIA 375.'
+console.log(myclass)
+
+// This is more code after making myClassError
+
+console.log(myClass.length())
